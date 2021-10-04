4afa9aaa2a6a0523c04e3c85f602b18de6652a23
KHELIFA Yassine <prostam27@gmail.com>
2021-10-04 23:03:39 +0200

    Setting Up VueJS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <script src="{{ mix('js/app.js') }}" defer></script>
 
         <title>Laravel</title>
         
     </head>
     <body class="antialiased">
+        <div id="app">
+
+        </div>
        
     </body>
 </html>
